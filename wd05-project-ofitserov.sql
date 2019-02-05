ec253609ee64bdfc1ba304fa7b9dd223a66211a8
Evgeniy Ofitserov <44874156+evgeniy-ofitserov@users.noreply.github.com>
2019-02-05 13:24:30 +0300

    Доработка №2 БД

--- wd05-project-ofitserov.sql
+++ wd05-project-ofitserov.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Хост: 127.0.0.1:3306
--- Время создания: Фев 04 2019 г., 17:10
+-- Время создания: Фев 05 2019 г., 13:23
 -- Версия сервера: 5.6.38
 -- Версия PHP: 5.5.38
 
@@ -30,7 +30,7 @@ SET time_zone = "+00:00";
 
 CREATE TABLE `users` (
   `id` int(11) UNSIGNED NOT NULL,
-  `email` varchar(191) COLLATE utf8mb4_unicode_520_ci DEFAULT NULL,
+  `email` varchar(255) COLLATE utf8mb4_unicode_520_ci DEFAULT NULL,
   `role` varchar(191) COLLATE utf8mb4_unicode_520_ci DEFAULT NULL,
   `password` varchar(191) COLLATE utf8mb4_unicode_520_ci DEFAULT NULL,
   `name` varchar(191) COLLATE utf8mb4_unicode_520_ci DEFAULT NULL,
@@ -49,7 +49,7 @@ CREATE TABLE `users` (
 
 INSERT INTO `users` (`id`, `email`, `role`, `password`, `name`, `subname`, `country`, `city`, `avatar`, `avatar_small`, `recovery_code`, `recovery_code_times`) VALUES
 (3, 'root@mail.ru', 'admin', '$2y$10$RO9mq9cACUv6nhdYgCqFZuW8ErsFWnEHFg.Kgf69p73e9ySaC0dti', 'евгений', 'офицеров', 'Россия', 'Москва', '-399096898.jpg', 'size48x48--399096898.jpg', 'HOQkwh8sWLc2X3C', 3),
-(4, 'ываыва@dsfsf.ru', 'user', '$2y$10$LUL1tofuCkxNLcH1jnJNrenJzqegv5FpoPSCTdkkg33/QWX3OMBaO', 'sdvsdv', 'sdvs', 'sdvsdv', 'sdvsdv', '318607220.jpg', 'size48x48-318607220.jpg', NULL, NULL);
+(16, 'lolo@ma.ru', 'user', '$2y$10$Rx71RkezJkCQGKXFgrpFjeVpcrzX0cMDzZNp/E6ovIrEaxHCzIQrq', 'Евгений', 'Еремин', 'Россия', 'Самара', '450845523.jpg', 'size48x48-450845523.jpg', NULL, NULL);
 
 --
 -- Индексы сохранённых таблиц
@@ -69,7 +69,7 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT для таблицы `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=17;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
